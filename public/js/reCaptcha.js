7ed276ff66aa20359ace75e2ee40c0f50c8ae767
Drakx116 <alexandre2.guyot@gmail.com>
2019-06-07 19:16:09 +0200

    SupJeans 1.0

--- public/js/reCaptcha.js
+++
@@ -1,7 +0,0 @@
-grecaptcha.ready(function () {
-    alert("OK");
-    grecaptcha.execute('6LdiYqAUAAAAADR3JpRDAPSf9lVykf2x07kFWswC', { action: 'contact' }).then(function (token) {
-        var recaptchaResponse = document.getElementById('recaptchaResponse');
-        recaptchaResponse.value = token;
-    });
-});
